443867b9d5ac4efd1b3c099113c89af366fe17da
darshan <darsh2605@gmail.com>
2024-02-15 00:24:41 +0530

    Done with the setup 1. Firebase 2. Ad mob 3. Pubsecp 4. File setup 5. Build Gradle

---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+const kprimarycolor = Color(0xff1C3249);
+const ksecondcolor = Color(0xff3ba2ff);
+const kthirdcolor = Color(0xff006ac0);
+const ksplashback = Color(0xffD48E7E);
+const kback = Color(0xffD48E7E);
+const backcolor = Color(0xFFD48E7E);
+const backcolor_2 = Color(0xFFFFFFFF);
+
+
+
+var androidAppShareLink = "https://play.google.com/store/apps/details?id=com.darshankomu.hindi_tongue_twister";
+
+var androidShareText = "Download Tongue Twister in Hindi app from below link";
+
+var androidAppIdValue = "com.darshankomu.hindi_tongue_twister";
+
+var iOSAppIdValue = "AddiOSAppId";
